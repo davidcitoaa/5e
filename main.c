1505a373f22ed57392754551fba772a98068f647
davidcitoaa <90219589+davidcitoaa@users.noreply.github.com>
2022-04-11 00:26:39 +0300

    task 4

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include "string/tasks/removeNonLetters.h"
 #include "string/tasks/removeExtraSpaces.h"
 #include "string/tasks/digitToStart.h"
+#include "string/tasks/replaceNumbersWithSpaces.h"
 
 int main() {
     test_removeNonLetters();
     test_removeExtraSpaces();
     test_digitToStart();
+    test_replacesNumbersWithSpaces_task4();
 
     return 0;
 }
--- string/tasks/digitToStart.h
+++ string/tasks/digitToStart.h
@@ -3,7 +3,7 @@
 
 #include "../string_.h"
 
-void digitToStartLetterToEnd(WordDescriptor word) {
+void digitToStart(WordDescriptor word) {
     char *endStringBuffer = copy(word.begin, word.end, _stringBuffer);
     char *recPosition = copyIf(_stringBuffer,
                                endStringBuffer,
@@ -11,12 +11,12 @@ void digitToStartLetterToEnd(WordDescriptor word) {
     copyIf(_stringBuffer, endStringBuffer, recPosition, isalpha);
 }
 
-void digitToStart(char *s) {
+void digitToStartLetterToEndt(char *s) {
     char *beginSearch = s;
     WordDescriptor word;
 
     while (getWord(beginSearch, &word)) {
-        digitToStartLetterToEnd(word);
+        digitToStart(word);
 
         beginSearch = word.end;
     }
@@ -26,25 +26,25 @@ void test_digitToStart1() {
     char s[] = "Hibro123";
 
 
-    digitToStart(s);
+    digitToStartLetterToEndt(s);
     ASSERT_STRING("123Hibro", s);
 }
 
 void test_digitToStart2() {
     char s[] = "Hibro1h23";
-    digitToStart(s);
+    digitToStartLetterToEndt(s);
     ASSERT_STRING("123Hibroh", s);
 }
 
 void test_digitToStart3() {
     char s[MAX_STRING_SIZE] = "Hibro1h23 j0";
-    digitToStart(s);
+    digitToStartLetterToEndt(s);
     ASSERT_STRING("123Hibroh 0j", s);
 }
 
 void test_digitToStart4() {
     char s[] = "Hibro1h23 j0h41";
-    digitToStart(s);
+    digitToStartLetterToEndt(s);
     ASSERT_STRING("123Hibroh 041jh", s);
 }
 
